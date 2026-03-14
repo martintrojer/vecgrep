f8cd1d949590d2b17afc5e33fb861b19c518e4c4
Kris Jenkins <krisajenkins@gmail.com>
2026-03-14 18:50:55 +0000

    Adding a nix flake for the build environment.

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              cargo-generate
+              cargo-watch
+              rustup
+              rust-analyzer
+            ];
+          };
+      });
+}
